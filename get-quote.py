b718cf2d3f2add11f05d84f1afbd125bb2fff806
Bujjigit <bujjirer@gmail.com>
2020-06-19 09:48:24 +0530

    add code for pmompting a new code and the same will append at the end

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,16 @@
 def Test():
   #print("Keep it logically awesome.")
+  quote = input("Enter new quote: ") 
+  f = open("quotes.txt", "a")
+  f.write(quote)
+  f.close()
 
   f = open("quotes.txt")
   quotes = f.readlines()
   new_quotes=[x[:-1] for x in quotes]
   f.close()
 
-  print(new_quotes[0:2])
+  print(new_quotes[-1])
 
 if __name__== "__main__":
   Test()
